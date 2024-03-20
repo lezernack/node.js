81c5e3149758f1897b27442f5f5dec24f906e18c
lezernack <luks.ezck@gmail.com>
2024-03-20 14:17:54 -0600

    trying to make new route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const myController = require("../controllers");
 const routes = require("express").Router();
 
 routes.get("/", myController.awesomeFunction);
-routes.get("/", myController.ttech);
+routes.get("/ttech", myController.ttech);
 
 module.exports = routes;
