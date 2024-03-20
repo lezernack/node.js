66f0e496d2901282fdfde85f7fee17923bb05435
lezernack <luks.ezck@gmail.com>
2024-03-20 16:43:47 -0600

    changed a line of code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const routes = require("express").Router();
 const route = require("express").Router();
 
 routes.get("/", myController.awesomeFunction);
-route.get("/ttech", myController.ttechFunction);
+route.get("/", myController.ttechFunction);
 
 module.exports = routes;
 module.exports = route;
