dace4549f692983dc916555b569d5e516de1cf7d
lezernack <luks.ezck@gmail.com>
2024-03-20 16:45:42 -0600

    deleted some code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 const myController = require("../controllers");
 const routes = require("express").Router();
-const route = require("express").Router();
 
 routes.get("/", myController.awesomeFunction);
-route.get("/", myController.ttechFunction);
 
 module.exports = routes;
-module.exports = route;
