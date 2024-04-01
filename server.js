15f7d7244e3cb0d72bd700467e3f38e3790b0c8d
lezernack <luks.ezck@gmail.com>
2024-04-01 12:20:57 -0600

    changed the code

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const app = express();
 const PORT = 3000;
 
 app.use("/", require("./routes"));
-app.use("/ttech", require("./routes"));
 
 app.listen(PORT, console.log(`Test server running on port: ${PORT}`));
